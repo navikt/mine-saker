4da8eb6bc6416f409b3e722e51f78ada0578bb82
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-03-30 12:09:41 +0200

    Fiks warning på unused-wars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from "@sentry/browser";
 import App from "./App";
 import Providers from "./providers/Providers";
 import "./index.less";
@@ -8,8 +7,6 @@ import "./index.less";
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
   worker.start();
-} else {
-  // Sentry.init({ dsn: "TODO: Legg inn din  egen sentry url" });
 }
 
 ReactDOM.render(
