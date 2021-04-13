1a25790b010c56db44aa3f6c3b26146bc838f6ad
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-04-13 14:23:22 +0200

    La til et ikon for dokument

---
+++ src/assets/DokumentIkon.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DokumentIkon = () => (
+  <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="40" cy="40" r="40" fill="#99C2E8" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M43.3333 20L55 31.6667V60H25V20H43.3333ZM40 23.3333H28.3333V56.6667H51.6667V33.3333H40V23.3333ZM45 48.3333V51.6667H31.6667V48.3333H45ZM48.3333 41.6667V45H31.6667V41.6667H48.3333ZM41.6667 35V38.3333H31.6667V35H41.6667ZM43.3333 24.7167V30L48.6167 29.9983L43.3333 24.7167Z"
+      fill="#3E3832"
+    />
+  </svg>
+);
+
+export default DokumentIkon;
--- src/components/dokumentliste/DokumentListe.js
+++ src/components/dokumentliste/DokumentListe.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useQueries } from "react-query";
 import { Systemtittel } from "nav-frontend-typografi";
-import SaksIkon from "../../assets/SaksIkon";
 import Veilederpanel from "nav-frontend-veilederpanel";
 import { fetchBySakstemaKode } from "../../api";
 import "./Dokumentliste.less";
+import DokumentIkon from "../../assets/DokumentIkon";
 
 const DokumentListe = ({ sakstemaer }) => {
   const queries = useQueries(
@@ -19,7 +19,7 @@ const DokumentListe = ({ sakstemaer }) => {
   console.log(queries);
 
   return (
-    <Veilederpanel type="plakat" kompakt svg={<SaksIkon />}>
+    <Veilederpanel type="plakat" kompakt svg={<DokumentIkon />}>
       <div className="dokumentliste-tittel">
         <Systemtittel>Dokumentliste</Systemtittel>
       </div>
