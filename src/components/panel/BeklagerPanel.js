0e638538336f0553486253cde43883f9cb7d4b55
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-05-26 13:21:20 +0200

    Oppdatert tekst med beklagelse

--- src/components/panel/BeklagerPanel.js
+++ src/components/panel/BeklagerPanel.js
@@ -1,28 +1,18 @@
 import React from "react";
 import { Normaltekst } from "nav-frontend-typografi";
 import AlertStripe from "nav-frontend-alertstriper";
+import Lenke from "nav-frontend-lenker";
+import { kontaktOssUrl } from "../../urls";
 import "./BeklagerPanel.less";
 
 const BeklagerPanel = () => {
   return (
     <AlertStripe type="info" className="beklager-panel">
-      <Normaltekst className="blokk-s">
-        <b>Vi beklager at du ikke får se eller åpnet alle dokumentene i saken din.</b>
-      </Normaltekst>
       <Normaltekst>
-        Mine saker er en innsynstjeneste som inneholder søknader, vedtaksbrev og andre viktige dokumenter i saken din.
-        Det er imidlertid to typer dokumenter vi ennå ikke viser i Mine saker, og som er merket med «Adgang
-        forbudt»-skilt når du logger inn på nav.no. Dette er:
-        <ul className="beklager-panel__liste">
-          <li>- Dokumenter som du har sendt via vanlig post til NAV.</li>
-          <li>
-            - Dokumenter som en tredjepart (leger/andre behandlere, advokater, verger, fullmektiger og lignende) har
-            sendt inn, og som angår saken din.
-          </li>
-        </ul>
-        Vi jobber for at dokumenter du sender inn via vanlig post om ikke alt for lenge skal bli synlige i Mine saker,
-        uten at vi nøyaktig kan si når det vil skje. Når det gjelder innsyn i dokumenter sendt inn av en tredjepart må
-        vi bruke mer tid på å finne en god løsning.
+        Dokumenter som du har sendt inn via vanlig post til NAV og dokumenter sendt inn via tredjepart (leger/andre
+        behandlere, advokater, verger, fullmektiger og lignende), vises dessverre ikke her. Vi jobber for å finne en
+        løsning, og beklager ulempene dette måtte medføre. Ta <Lenke href={kontaktOssUrl}>kontakt</Lenke> dersom det er
+        noe du lurer på noe.
       </Normaltekst>
     </AlertStripe>
   );
