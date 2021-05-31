97fef89bd9b354a8571f55c0969fa15a82ec412c
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-05-31 18:19:50 +0200

    Fjernet preflight sjekk på første nivå til fordel for feilhåndtering i queries

--- src/components/chevronlenke/sakstemalenke/Sakstemalenke.js
+++ src/components/chevronlenke/sakstemalenke/Sakstemalenke.js
@@ -2,20 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { HoyreChevron } from "nav-frontend-chevron";
 import { Link } from "react-router-dom";
-import { mineSakerUrl } from "../../../urls";
-import checkAuthenticationStatus from "../../../utils/redirect";
 import "nav-frontend-lenker";
 import "./Sakstemalenke.less";
 
 const Sakstemalenke = ({ tekst, kode }) => {
-  const redirectUrl = `${mineSakerUrl}/tema/${kode}`;
-
-  const onClick = async () => {
-    await checkAuthenticationStatus(redirectUrl);
-  };
-
   return (
-    <Link className="lenke sakstemalenke" onClick={onClick} to={`/person/mine-saker/tema/${kode}`}>
+    <Link className="lenke sakstemalenke" to={`/person/mine-saker/tema/${kode}`}>
       <HoyreChevron className="sakstemalenke__chevron" /> {tekst}
     </Link>
   );
