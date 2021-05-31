7c1796960bca8ca98752efda855b49199be69af6
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-05-31 15:59:50 +0200

    Fjernet sjekk av innloggingsstatus for hver sidevisning siden det gjøres på lenkenivå

--- src/components/authentication/Authentication.js
+++ src/components/authentication/Authentication.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { useQuery, useQueryClient } from "react-query";
+import React from "react";
+import { useQuery } from "react-query";
 import fetchData from "../../api";
 import { mineSakerUrl, statusUrl } from "../../urls";
 import { redirectToIdPorten } from "../../utils/redirect";
@@ -8,12 +7,6 @@ import Spinner from "../spinner/Spinner";
 
 const Authentication = ({ children }) => {
   const { data: status, isLoading, isError } = useQuery(statusUrl, fetchData);
-  const queryClient = useQueryClient();
-  const location = useLocation();
-
-  useEffect(() => {
-    queryClient.invalidateQueries(statusUrl);
-  }, [location, queryClient]);
 
   if (isLoading) {
     return <Spinner message="Logger inn..." />;
