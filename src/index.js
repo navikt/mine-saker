f2f8b7c9451bc68aed0ab7b88089da55cbde00d5
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-06-04 09:37:24 +0200

    Henter inn dekoratøren for lokal kjøring

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Providers from "./providers/Providers";
 import Authentication from "./components/authentication/Authentication";
+import { injectDecoratorClientSide } from "@navikt/nav-dekoratoren-moduler/csr";
 import "./index.less";
+
+const injectDecorator = async () => {
+  await injectDecoratorClientSide({
+    env: "dev",
+  });
+};
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
   worker.start();
+  injectDecorator();
 }
 
 ReactDOM.render(
