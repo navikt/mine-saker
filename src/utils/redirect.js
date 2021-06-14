f0243a38262a4ede7ee9a74bb32f9b27f286815d
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-06-14 16:02:19 +0200

    Prøver uten tokenx i prod

--- src/utils/redirect.js
+++ src/utils/redirect.js
@@ -1,10 +1,8 @@
 import { fetchStatus } from "../api";
-import { loginserviceUrl, loginUrl } from "../urls";
 
-export const redirectToIdPorten = (redirectUri) => {
-  const loginserviceUri = `${loginserviceUrl}&redirect=${redirectUri}`;
-
-  window.location.assign(`${loginUrl}?redirect_uri=${loginserviceUri}`);
+export const redirectToIdPorten = () => {
+  // const loginserviceUri = `${loginserviceUrl}&redirect=${redirectUri}`;
+  // window.location.assign(`${loginUrl}?redirect_uri=${loginserviceUri}`);
 };
 
 const checkAuthenticationStatus = async (redirectUrl) => {
