c801ab5e45624f69cbbea0ecca073453335844f7
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-06-14 15:44:08 +0200

    Reverter testing av redirect til loginservice

--- src/utils/redirect.js
+++ src/utils/redirect.js
@@ -1,10 +1,10 @@
 import { fetchStatus } from "../api";
-import { loginserviceUrl } from "../urls";
+import { loginserviceUrl, loginUrl } from "../urls";
 
 export const redirectToIdPorten = (redirectUri) => {
-  // const loginserviceUri = `${loginserviceUrl}&redirect=${redirectUri}`;
+  const loginserviceUri = `${loginserviceUrl}&redirect=${redirectUri}`;
 
-  window.location.assign(`${loginserviceUrl}&redirect=${redirectUri}`);
+  window.location.assign(`${loginUrl}?redirect_uri=${loginserviceUri}`);
 };
 
 const checkAuthenticationStatus = async (redirectUrl) => {
