e1ec439d7b75b0fd65961769b40eb7c8a89563c5
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-06-21 16:18:13 +0200

    La til økonomisk sosialhjelp som sakstema

--- src/components/chevronlenke/sakstemalenke/Sakstemalenke.js
+++ src/components/chevronlenke/sakstemalenke/Sakstemalenke.js
@@ -4,8 +4,19 @@ import { HoyreChevron } from "nav-frontend-chevron";
 import { Link } from "react-router-dom";
 import "nav-frontend-lenker";
 import "./Sakstemalenke.less";
+import Lenke from "nav-frontend-lenker";
 
 const Sakstemalenke = ({ tekst, kode }) => {
+  if (kode === "KOM") {
+    const url = "https://www-q1.dev.nav.no/sosialhjelp/innsyn";
+
+    return (
+      <Lenke className="dokumentlenke" href={url}>
+        <HoyreChevron className="dokumentlenke__chevron" /> {tekst}
+      </Lenke>
+    );
+  }
+
   return (
     <Link className="lenke sakstemalenke" to={`/person/mine-saker/tema/${kode}`}>
       <HoyreChevron className="sakstemalenke__chevron" /> {tekst}
