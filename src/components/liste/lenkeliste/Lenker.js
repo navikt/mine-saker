73529aea6d71f43c0d6e159c9c9a19ca6f5b1369
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-08-17 14:59:36 +0200

    Legger lenke til mine-dagpenger i sakstemasiden
    Co-authored-by: Amirhlotfi <Amirhossien.lotfi@nav.no>
    Co-authored-by: eivinne <eivinn.eilertsen@nav.no>

--- src/components/liste/lenkeliste/Lenker.js
+++ src/components/liste/lenkeliste/Lenker.js
@@ -1,4 +1,5 @@
 import {
+  dagpengerUrl,
   kontaktOssUrl,
   lastOppVedleggUrl,
   opplysningspliktUrl,
@@ -26,6 +27,33 @@ const generelleLenker = [
   {
     url: lastOppVedleggUrl,
     tekst: "Last opp vedlegg",
+  },
+];
+
+export const dagpengeLenker = [
+  {
+    url: kontaktOssUrl,
+    tekst: "Kontakt oss",
+  },
+  {
+    url: tilbakemeldingerUrl,
+    tekst: "Tilbakemeldinger",
+  },
+  {
+    url: opplysningspliktUrl,
+    tekst: "Opplysningsplikt",
+  },
+  {
+    url: saksbehandlingstiderUrl,
+    tekst: "Saksbehandlingstider",
+  },
+  {
+    url: lastOppVedleggUrl,
+    tekst: "Last opp vedlegg",
+  },
+  {
+    url: dagpengerUrl,
+    tekst: "Mine dagpenger",
   },
 ];
 
