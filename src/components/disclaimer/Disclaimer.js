34b498d96bb613694e7f03f896c7b4fc525d5f45
Eilertsen <eivinn.eilertsen@nav.no>
2021-10-01 15:41:32 +0200

    Tilpasset disclaimerens utseende og tekstlig innhold til å matche Dag Helge sine forslag i slack-thread.

--- src/components/disclaimer/Disclaimer.js
+++ src/components/disclaimer/Disclaimer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AlertStripe from 'nav-frontend-alertstriper';
-import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
-import {  useIntl } from 'react-intl';
+import { Normaltekst } from 'nav-frontend-typografi';
+import { useIntl } from 'react-intl';
 import Lenke from 'nav-frontend-lenker';
 import { gamleSaksoversiktUrl } from '../../urls';
 //import { GoogleAnalyticsAction, GoogleAnalyticsCategory, trackEvent } from '../../../utils/googleAnalytics';
@@ -10,35 +10,31 @@ const Disclaimer = () => {
 
     const translate = useIntl();
 
-    const undertittel = "disclaimer.undertittel";
+    const boldTekst = "disclaimer.bold-tekst";
     const tekst = "disclaimer.tekst";
     const lenketekst = "disclaimer.lenketekst";
 
     return (
 <div className="alertbox">
-    <AlertStripe type="advarsel" className="infomelding">
-        <Undertittel>
-            {translate.formatMessage({ 
-                id: undertittel, 
-                defaultMessage: "Denne siden er under utvikling"
-            })}
-        </Undertittel>
+    <AlertStripe type="info" className="infomelding" style={{display: "flex", flexDirection: "row"}}>
         <Normaltekst>
+            <b>
+                {translate.formatMessage({ 
+                    id: boldTekst, 
+                    defaultMessage: "Denne siden er under utvikling, "
+                })}
+            </b>
             {translate.formatMessage({ 
-                id: tekst, 
-                defaultMessage: "Her skal du kunne se dine saker hos NAV og dokumenter som tilhører sakene. Denne løsningen er forstatt under arbeid og skal erstatte en tidligere løsning for saksoversikt. Om du ikke finner sakene eller dokumentene du leter etter kan du forsøke "
-            })}
+                    id: tekst, 
+                    defaultMessage: "og er ikke helt ferdig enda. Hvis du ikke finner sakene eller dokumentene du leter etter kan du forsøke "
+                })}
             <Lenke
                 href={gamleSaksoversiktUrl}
-                /*onClick={() => trackEvent(
-                GoogleAnalyticsCategory.Varslinger,
-                GoogleAnalyticsAction.Innboks,
-                lenker.innboks.url,
-                )}*/
             >
                 {translate.formatMessage({
-                    id: lenketekst,
-                    defaultMessage: "den tidligere versjonen"})}
+                    id: lenketekst, 
+                    defaultMessage: "den tidligere versjonen."
+                })}
             </Lenke>
         </Normaltekst>
     </AlertStripe>
