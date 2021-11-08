9285a32626aebbeeb788d245c0f0385006e48439
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-11-08 15:39:22 +0100

    Bare vise dokument og vedlegg hvis brukerHarTilgang er eksplisitt true.
    Har sett i loggene at noen brukere får trykket på lenke de ikke skal ha tilgang til, håper dette fikser det.
    
    Co-authored-by: Atle Nordland <Atle.Nordland@nav.no>

--- src/components/liste/vedleggsliste/VedleggslisteItem.js
+++ src/components/liste/vedleggsliste/VedleggslisteItem.js
@@ -7,27 +7,24 @@ import { EtikettAdvarsel } from "nav-frontend-etiketter";
 import { dokumentUrl } from "../../../urls";
 
 const VedleggslisteItem = ({ journalpostId, dokumentId, brukerHarTilgang, tittel, children }) => {
-  
-  if(brukerHarTilgang === false) {
-    return(
+  if (brukerHarTilgang === true) {
+    return (
       <li className="vedleggsliste-item">
-      <Undertekst className="vedleggsliste-item-undertekst">
-        {tittel}
-      </Undertekst>
-      <EtikettAdvarsel className="maskert-etiketter__advarsel" mini>
-        Vedlegget kan ikke vises
-      </EtikettAdvarsel>
-    </li>
+        <Lenke className="vedleggslenke" href={`${dokumentUrl}/${journalpostId}/${dokumentId}`}>
+          <HoyreChevron className="vedleggslenke__chevron" /> {children}
+        </Lenke>
+      </li>
+    );
+  } else {
+    return (
+      <li className="vedleggsliste-item">
+        <Undertekst className="vedleggsliste-item-undertekst">{tittel}</Undertekst>
+        <EtikettAdvarsel className="maskert-etiketter__advarsel" mini>
+          Vedlegget kan ikke vises
+        </EtikettAdvarsel>
+      </li>
     );
   }
-
-  return (
-    <li className="vedleggsliste-item">
-      <Lenke className="vedleggslenke" href={`${dokumentUrl}/${journalpostId}/${dokumentId}`}>
-        <HoyreChevron className="vedleggslenke__chevron" /> {children}
-      </Lenke>
-    </li>
-  );
 };
 
 VedleggslisteItem.propTypes = {
