fe13040c7f60581067ee1f2cd9437301afc21b8c
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-02-22 12:48:30 +0100

    Egen spesifisert lenkeliste under om saksen for sakstema OMS.

--- src/hooks/useTemaException.js
+++ src/hooks/useTemaException.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const exceptions = ["DAG", "HJE", "FOR", "KOM", "AAP", "SYK", "IND", "SYM", "PEN", "UFO"];
+const exceptions = ["DAG", "HJE", "FOR", "KOM", "AAP", "SYK", "IND", "SYM", "PEN", "UFO","OMS"];
 
 const useTemaException = (temakode) => {
   const [isException, setIsException] = useState(false);
